25f600b51f8498f92bf0d96444d4623f520e478d
Miroslav Vitanov <mvvitanov@gmail.com>
2016-03-22 11:03:08 +0200

    Register formatters

--- src/Config/administr.listview.php
+++ src/Config/administr.listview.php
@@ -4,4 +4,8 @@ return [
     'date_format'       => 'd/m/Y',
     'time_format'       => 'H:i:s',
     'datetime_format'   => 'd/m/Y H:i:s',
+
+    'formatters'        => [
+        'yesno' => \Administr\ListView\Formatters\YesNoFormatter::class,
+    ]
 ];

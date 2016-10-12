4638d3767b955994396266bd20dcdcf361ffa5ef
Miroslav Vitanov <mvvitanov@gmail.com>
2016-10-12 14:48:36 +0300

    Remove references to $contextActions

--- src/Views/list.blade.php
+++ src/Views/list.blade.php
@@ -3,45 +3,36 @@
 <table {!! $attrs !!}>
 
     <thead>
-    <tr>
-        @foreach($columns as $column)
-            @if($column->visible())
-                @include('administr/listview::_column')
-            @endif
-        @endforeach
-
-        @if(count($contextActions) > 0)
-            <th></th>
-        @endif
-    </tr>
+        <tr>
+            @foreach($columns as $column)
+                @if($column->visible())
+                    @include('administr/listview::_column')
+                @endif
+            @endforeach
+        </tr>
     </thead>
 
     <tbody>
-    @foreach($values as $row)
-        @include('administr/listview::_row')
-    @endforeach
+        @foreach($values as $row)
+            @include('administr/listview::_row')
+        @endforeach
     </tbody>
 
     <tfoot>
-    <tr>
-        @foreach($columns as $column)
-            @if($column->visible())
-                @include('administr/listview::_column')
-            @endif
-        @endforeach
-
-        @if(count($contextActions) > 0)
-            <th></th>
-        @endif
-    </tr>
-
-    @if($paginationLinks)
         <tr>
-            <td colspan="@if(count($contextActions) > 0){{ count($columns) + 1 }}@else{{ count($columns) }}@endif">
-                {!! $paginationLinks !!}
-            </td>
+            @foreach($columns as $column)
+                @if($column->visible())
+                    @include('administr/listview::_column')
+                @endif
+            @endforeach
         </tr>
-    @endif
 
+        @if($paginationLinks)
+            <tr>
+                <td colspan="{{ count($columns) }}">
+                    {!! $paginationLinks !!}
+                </td>
+            </tr>
+        @endif
     </tfoot>
 </table>

72cd08ceccdf6246c21ae226944eb048870d9802
Kaukov <Kaukov@users.noreply.github.com>
2017-02-02 16:31:00 +0200

    Don't require local translations

--- src/Formatters/YesNoFormatter.php
+++ src/Formatters/YesNoFormatter.php
@@ -8,6 +8,6 @@ class YesNoFormatter implements Formatter
 {
     public function format($value)
     {
-        return (bool)$value ? '<span class="label label-success">' . trans('administr-listview::formatters.yesno.yes') . '</span>' : '<span class="label label-danger">' . trans('administr-listview::formatters.yesno.no') . '</span>';
+        return (bool)$value ? '<span class="label label-success">' . trans('formatters.yesno.yes') . '</span>' : '<span class="label label-danger">' . trans('formatters.yesno.no') . '</span>';
     }
 }
